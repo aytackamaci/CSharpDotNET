b45635047984082adf76221dcd8f88a442066aac
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-03-07 17:28:07 +0300

    BooleanOperators

---
+++ Chapter03/BooleanOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+/*
+^ operator returns true when one of two operands is true, but not both! 
+*/
+bool a = true;
+bool b = false;
+
+static bool DoStuff() 
+{
+    WriteLine("I'm here.");
+    return true;
+}
+
+WriteLine();
+WriteLine($"a & DoStuff() = {a & DoStuff()}");
+WriteLine($"b & DoStuff() = {b & DoStuff()}");
+
+WriteLine($"a && DoStuff() = {a && DoStuff()}");
+WriteLine($"b && DoStuff() = {b && DoStuff()}");
