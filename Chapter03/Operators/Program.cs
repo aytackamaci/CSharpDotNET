4a5958e7e8c62643b7f637242aa4aa807114b7bb
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-03-07 17:13:44 +0300

    Operators

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+int a = 3;
+int b = a++;
+int c = ++a;
+
+WriteLine($"a is {a}, b is {b} and c is {c}");
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
