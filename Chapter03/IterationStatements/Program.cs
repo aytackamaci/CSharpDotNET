dfa9ad9b0cbb6f2792aa3d49bdf5b3d59b51d285
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-03-08 18:37:12 +0300

    IterationStatements

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+string? password;
+int count = 0;
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+do
+{
+  Write("Enter your password: ");
+  password = ReadLine();
+  count++;
+}
+while ((password != "password") && count < 10);
+WriteLine("Correct!");
