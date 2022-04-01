1861aaeadb83d8ea7c7a77b57cc80e25ae0fdfbf
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-04-01 16:54:06 +0300

    instrumenting 1

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+using System.Diagnostics;
+
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.CreateText(Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+// text writer is buffered, so this option calls // Flush() on all listeners after writing Trace.AutoFlush = true;
+
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
