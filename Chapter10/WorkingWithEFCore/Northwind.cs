b0ae0c06810115167fe2011cb8a4c39bfec5fa2a
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-09-02 14:01:29 +0300

    chapter10

---
+++ Chapter10/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore; // DbContext, DbContextOptionsBuilder
+using static System.Console;
+namespace Packt.Shared;
+// this manages the connection to the database
+public class Northwind : DbContext
+{
+
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<Product>? Products { get; set; }
+
+    protected override void OnConfiguring(
+      DbContextOptionsBuilder optionsBuilder)
+    {
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            string path = Path.Combine(
+              Environment.CurrentDirectory, "Northwind.db");
+            WriteLine($"Using {path} database file.");
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+        else
+        {
+            string connection = "Data Source=.;" +
+              "Initial Catalog=Northwind;" +
+              "Integrated Security=true;" +
+              "MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+        .Property(category => category.CategoryName)
+        .IsRequired()
+        .HasMaxLength(15);
+
+        if (ProjectConstants.DatabaseProvider == "SQLite")
+        {
+            modelBuilder.Entity<Product>()
+            .Property(product => product.Cost)
+            .HasConversion<double>();
+        }
+    }
+}
---
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using Packt.Shared;
+using static System.Console;
+using Microsoft.EntityFrameworkCore;
+
+WriteLine($"Using {ProjectConstants.DatabaseProvider} database provider.");
+
+QueryingCategories();
+static void QueryingCategories()
+{
+    using (Northwind db = new())
+    {
+        WriteLine("Categories and how many products they have:");
+
+        IQueryable<Category>? categories = db.Categories?
+            .Include(c => c.Products);
+
+        if (categories is null)
+        {
+            WriteLine("No categories found.");
+            return;
+        }
+
+        foreach (Category c in categories)
+        {
+            WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+        }
+    }
+}
