21440824e01832e704c282e1b003e60b8d8b8889
Aytaç KAMACI <60390900+aytackamaci@users.noreply.github.com>
2022-10-07 13:56:47 +0300

    chapter12

---
+++ Chapter12/AsyncEnumerable/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+using System.Diagnostics; // Stopwatch
+
+await foreach (int number in GetNumbersAsync())
+{
+    WriteLine($"Number: {number}");
+}
+
+async static IAsyncEnumerable<int> GetNumbersAsync()
+{
+    Random r = new();
+    // simulate work
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(0, 10);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(10, 100);
+    await Task.Delay(r.Next(1500, 3000));
+    yield return r.Next(100, 1001);
+}
